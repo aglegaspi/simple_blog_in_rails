be0881bbddf236746257c9427df2563ec81d340f
Alexander George Legaspi <lex83nyc@gmail.com>
2018-11-12 16:24:54 -0500

    created Articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/edit'
+  get 'articles/destroy'
+  get 'articles/index'
+  get 'articles/show'
   get 'welcome/index'
+
+  resources :articles
 
   root 'welcome#index'
 end
