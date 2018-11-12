823243f49d42e7fc93a3fb718d377d0a394dc2d4
Alexander George Legaspi <lex83nyc@gmail.com>
2018-11-12 18:19:44 -0500

    saving data in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,22 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    # render plain: params[:article].inspect
+    # this renders the parameters in plain text and displays in view
+
+    @article = Article.new(article_params)
+    # every Rails model can be initialized with respective attributes which are
+    # are mapped to the respective database column i.e. title and text.
+    # Article.new refers to the class name in models/article.rb
+    # class names in Ruby begin with a capital letter in Ruby.
+
+    @article.save
+    # this saves the model in the database
+    # this returns a boolean whether the article was saved or not.
+
+    redirect_to @article
+    # we redirect user to the show action.
+
   end
 
   def update
@@ -19,4 +35,11 @@ class ArticlesController < ApplicationController
 
   def show
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+    # this makes the parameters being passed private and restricted to only these params
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/update'
-  get 'articles/edit'
-  get 'articles/destroy'
-  get 'articles/index'
-  get 'articles/show'
+  
   get 'welcome/index'
 
   resources :articles
